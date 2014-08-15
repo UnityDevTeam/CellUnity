fb656439331ed0f1b53bd97d9725ed9b0d8cd998
Daniel G <daniel@xa1.at>
2014-08-15 12:21:54 +0200

    diffusion test

---
+++ UnityProject/Assets/Scripts/Test/Diffusion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CellUnity.Test
+{
+	public class Diffusion : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public float Intensity = 1f;
+
+		private float GetRandomValue()
+		{
+			return Intensity * (Random.value - 0.5f);
+		}
+
+		void FixedUpdate()
+		{
+			Vector3 force = new Vector3 (GetRandomValue (), GetRandomValue (), GetRandomValue ());
+			rigidbody.AddForce(force);
+		}
+	}
+}
